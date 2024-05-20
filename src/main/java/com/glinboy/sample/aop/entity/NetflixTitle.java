7c3d417c33104ed6accea12aded42b9e505df78c
GLinBoy <glinboy@live.com>
2024-05-20 11:23:19 +0200

    👌 IMPROVE: Converted Entity Java record to Java class
    resolved #32

--- src/main/java/com/glinboy/sample/aop/entity/NetflixTitle.java
+++ src/main/java/com/glinboy/sample/aop/entity/NetflixTitle.java
@@ -1,49 +1,54 @@
 package com.glinboy.sample.aop.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "netflix_title")
-public record NetflixTitle(
+public class NetflixTitle {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.SEQUENCE)
-	Long id,
+	private Long id;
 
 	@Column(name = "show_id", length = 5)
-	String showId,
+	private String showId;
 
 	@Column(name = "show_type", length = 7)
-	String type,
+	private String type;
 
 	@Column(name = "title", length = 104)
-	String title,
+	private String title;
 
 	@Column(name = "director", length = 208)
-	String director,
+	private String director;
 
 	@Column(name = "show_cast", length = 771)
-	String cast,
+	private String cast;
 
 	@Column(name = "country", length = 123)
-	String country,
+	private String country;
 
 	@Column(name = "date_added", length = 19)
-	String dateAdded,
+	private String dateAdded;
 
 	@Column(name = "release_year", length = 4)
-	String releaseYear,
+	private String releaseYear;
 
 	@Column(name = "rating", length = 8)
-	String rating,
+	private String rating;
 
 	@Column(name = "duration", length = 10)
-	String duration,
+	private String duration;
 
 	@Column(name = "listed_in", length = 79)
-	String listedIn,
+	private String listedIn;
 
 	@Column(name = "description", length = 248)
-	String description
-) {
+	private String description;
 }
