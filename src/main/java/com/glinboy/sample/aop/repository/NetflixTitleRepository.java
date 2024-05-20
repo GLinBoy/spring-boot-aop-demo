7112417904cf65998ef6498f94f3eb60bf330cef
GLinBoy <glinboy@live.com>
2024-05-20 11:24:56 +0200

    👌 IMPROVE: Added a method to get a title by its Show ID

--- src/main/java/com/glinboy/sample/aop/repository/NetflixTitleRepository.java
+++ src/main/java/com/glinboy/sample/aop/repository/NetflixTitleRepository.java
@@ -1,7 +1,12 @@
 package com.glinboy.sample.aop.repository;
 
 import com.glinboy.sample.aop.entity.NetflixTitle;
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NetflixTitleRepository extends JpaRepository<NetflixTitle, Long> {
+	
+	Optional<NetflixTitle> findOneByShowId(String showId);
 }
