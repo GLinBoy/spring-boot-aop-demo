69f9fc34bdcda777abeaf15f7819bdf64650f1a1
GLinBoy <glinboy@live.com>
2024-11-02 11:29:58 +0100

    🐛 FIX: Fix swagger redirect

--- src/main/java/com/glinboy/sample/aop/web/controller/HomeController.java
+++ src/main/java/com/glinboy/sample/aop/web/controller/HomeController.java
@@ -4,13 +4,15 @@ import java.io.IOException;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import jakarta.servlet.http.HttpServletResponse;
 
 @Controller
+@RequestMapping("/")
 public class HomeController {
 
-	@GetMapping(value = "/")
+	@GetMapping
 	public void redirect(HttpServletResponse response) throws IOException {
 		response.sendRedirect("/swagger-ui.html");
 	}
